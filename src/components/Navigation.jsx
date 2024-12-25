3a400bcfd25c06b71d6275e022a40a367dcf8560
Shivvy-The-Coder <krshivam455@gmail.com>
2024-12-25 13:54:57 +0530

    codes has been added

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+
+
+const Navigation =()=>
+{
+    return(
+        <nav className="container">
+          <div className="logo">
+            <img src="./images/brand_logo.png" alt="" />
+          </div>
+          <ul>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Location</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <button>logn</button>
+        </nav>
+    );
+}
+
+export default Navigation;
